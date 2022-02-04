a0b6be7b3b361b4ec69aaea7679859ef8499138b
SamuelGomezG <samgomezgu@unal.edu.co>
2022-02-03 22:08:57 -0500

    - Samuel: Primera versión del juego. Añadí la clase MainFrame que contiene el frame donde se mostrarán los elementos de la GUI. Allí están dos paneles y 2 labels. Añadí la clase DisplayGuide que es una guía visual de cómo se verá el menú de las cartas. Añadí la clase Card Añadí el paquete sources para poner allí las imágenes y demás recursos que se lleguen a usar.

---
+++ src/Data/Card.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Data;
+
+/**
+ *
+ * @author samyg
+ */
+public abstract class Card {
+    String character;
+    String situation;
+
+    public String getCharacter() {
+        return character;
+    }
+
+    public void setCharacter(String character) {
+        this.character = character;
+    }
+
+    public String getSituation() {
+        return situation;
+    }
+
+    public void setSituation(String situation) {
+        this.situation = situation;
+    }
+    
+}
---
+++ src/javaapplication4/JavaApplication4.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication4;
+
+import GUI.MainFrame;
+
+/**
+ *
+ * @author samyg
+ */
+public class JavaApplication4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MainFrame mainf = new MainFrame();
+        mainf.runMainFrame();
+        
+    }
+    
+}
